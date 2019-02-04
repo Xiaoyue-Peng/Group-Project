c3fe4a4a1f08023b3d5b9f82bf89f93d63ca2880
Dongang Ji <dongang.ji@kcl.ac.uk>
2019-02-04 13:34:48 +0000

    Initial Commit

---
+++ Sync/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sync
+//
+//  Created by 纪东昂 on 04/02/2019.
+//  Copyright © 2019 trying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
