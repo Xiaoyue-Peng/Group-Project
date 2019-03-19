7a2414c3d5ed216c84fea620902a7ba34b8d5950
XIAOYUE PENG <k1896845@kcl.ac.uk>
2019-03-19 12:41:11 +0000

    Add files via upload

--- web develop/application/index/controller/Edit.php
+++ web develop/application/index/controller/Edit.php
@@ -11,29 +11,64 @@ use think\validate;
 class Edit extends Controller
 {
 	public function index(){
-		$filename= $_GET['filename'];
-		$res = Db::table('file_tb')->where('file_name',$filename)->find();
-
+		$fileid = $_GET['fileid'];
+		$res = Db::table('file_tb')
+		->where('file_id',$fileid)->find();
+		$old_version = Db::table('file_tb')
+		->where('file_ini_id',$res['file_ini_id'])
+		->select();
 		return $this->fetch('edit',[
-			'filename' => $filename,
+			'file_ini_id'=>$res['file_ini_id'],
+			'filename' => $res['file_name'],
 			'content'=>$res['content'],
     		'lastupdate' => $res['update_time'],
             'creator' => $res['create_usr'],
-            'file_id' => $res['file_id']
+            'file_id' => $res['file_id'],
+            'pre_id' =>$res['pre_id'],
+            'old_version'=>$old_version
     	]);
 	}
 
 	public function update(Request $request){
 		$post=$request->param();
+		dump($post);
 		if ($post){
-			Db::table('file_tb')->where('file_id',$post['file_id'])->update([
-				'content'=>$post['content_update'],
-				'update_usr'=>session('username')
-			]);
-			return $this->success('success');
+			$pre_id_2 = Db::table('file_tb')
+			->where('file_id',$post['file_id'])
+			->field('pre_id')
+			->find();
+			if ($post['pre_id_1'] == implode($pre_id_2))
+			{
+				// Db::table('file_tb')
+				// ->where('file_id',$post['file_id'])
+				// ->update([
+				// 	'file_name'=>$post['filename_update'],
+				// 	'content'=>$post['content_update'],
+				// 	'update_usr'=>session('username'),
+				// ]);
+				 File_tb::create([
+                        'file_name'=>$post['filename_update'],
+                        'file_ini_id' =>$post['file_ini_id'],
+                        'content'=>$post['content_update'],
+                     	'create_usr'=>$post['creator'],
+                     	'update_usr'=>session('username'),
+                     	'pre_id'=> $post['pre_id_1']+1 
+                    ]);
+
+				// Db::table('file_tb')
+				// ->where('file_ini_id',$post['file_ini_id'])
+				// ->setInc('pre_id');
+				return $this->redirect('./index');
 		}else{
-			return $this->error('failed');
+			//版本错误，返回检测网站
+			return $this->error('id');
 		}
 	}
+	else{
+		
+		return $this->error('failed');
+	}
+	}
+	
     
 }
--- web develop/application/index/controller/Index.php
+++ web develop/application/index/controller/Index.php
@@ -4,28 +4,46 @@ namespace app\index\controller;
 use think\controller;
 use think\Db;
 use app\index\model\Usr_tb;
+use app\index\model\File_tb;
+use think\file;
+use think\request;
 //use think\Debug;
 //use Config;
 
-
-
 class Index extends controller
 {
- 
     public function index()
     {
-    	return $this->fetch('index',[
-    		'username' => 'lala',
-    		'filename' => 'file1',
-    		'lastupdate' => '1day'
-    	]);
-    }
+        // $file_own=Db::table('file_tb')
+        // ->where('create_usr',session('username'))
+        // ->group('file_ini_id')
+        // ->field('file_ini_id')
+        // ->select();
+        // dump($file_own);
 
-    	
+        $file_own=Db::table('file_tb')
+        ->where('create_usr',session('username'))
+        ->where('file_ini_id', field('file_ini_id'))
+        // ->field('file_ini_id')
+        // ->group('file_ini_id')
+        ->select();
+        dump($file_own);
+       
+
+        return $this->fetch('index',[
+            'file_own' => $file_own 
+        ]);
+        //主页文件传输
+    //     for ($i=0;$i<=3;$i++){ 
+    //return $file_own;   
+    }
+   
+
+    
     public function upload()
     {
         //判断文件上传是否出错
-        dump($_FILES);
+        $file=$this->request->file("file");
         if($_FILES["file"]["error"])
         {
             echo $_FILES["file"]["error"];
@@ -33,27 +51,27 @@ class Index extends controller
         else
         {
             //控制上传的文件类型，大小
-            if($_FILES["file"]["type"]=="text/plain"&&$_FILES["file"]["size"]<1024000)
-            {
-                //找到文件存放位置，注意tp5框架的相对路径前面不用/
-                //这里的filename进行了拼接，前面是路径，后面从date开始是文件名
-                //我在static文件下新建了一个file文件用来存放文件，要注意自己建一个文件才能存放传过来的文件
-                $file_exists=Db::table('file_tb')->where('file_name',$_FILES["file"]["name"])->find();
-            
-                //判断文件是否存在
+            if($_FILES["file"]["type"]=="text/plain"||$_FILES["file"]["type"]=="application/octet-stream"&&$_FILES["file"]["size"]<1024000)
+            {            
+                $file_exists=Db::table('file_tb')->where('file_name',$_FILES["file"]["name"])->find();      
                 if ($file_exists)
                 {
-                    echo "该文件已存在！";
+                    echo "File exists!";
                     //选择是否替换
                 }
                 else
                 {
-            
-                    Db::table('file_tb')->insert([
+                    $new_file = $file->move('./static/upload');
+                    $content=file_get_contents($new_file->getpathName());
+                    // Db::table('file_tb')->insert([
+                    File_tb::create([
                         'file_name'=>$_FILES["file"]["name"],
-                        // 'content'=>$_FILES["file"]["content"],
-                     'create_usr'=>$session.username
+                        'file_ini_id' =>rand(),
+                        'content'=>$content,
+                     'create_usr'=>session('username'),
                     ]);
+                    
+                    return $this->success("upload success!");
                 }
             }
             else
@@ -62,6 +80,7 @@ class Index extends controller
             }
         }
     }
-
-    
+ 
 }
+?>
+    
